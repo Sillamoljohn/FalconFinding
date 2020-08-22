6dc3913ddd169d7d51222f5f29d37f6ca9f4e285
Silla  John <sillajohn333@gmail.com>
2020-08-22 20:04:39 +0530

    Vechile listing,planent listing and count for vechile is updated

--- src/app/find-falcon/find-falcon.component.ts
+++ src/app/find-falcon/find-falcon.component.ts
@@ -23,6 +23,11 @@ export class FindFalconComponent implements OnInit {
   isDestination2: boolean = false;
   isDestination3: boolean = false;
   isDestination4: boolean = false;
+  vechile1PrevId;
+  vechile2PrevId;
+  vechile3PrevId;
+  vechile4PrevId;
+
 
   constructor(
     private commonService: CommonServiceService,
@@ -41,9 +46,9 @@ export class FindFalconComponent implements OnInit {
   loadupdatesForm(): void {
     this.findFalconForm = this.fb.group({
       destination_1: [{ name: "select", distance: 500 }],
-      destination_2: ["select"],
-      destination_3: ["select"],
-      destination_4: ["select"],
+      destination_2: [{ name: "select", distance: 500 }],
+      destination_3: [{ name: "select", distance: 500 }],
+      destination_4: [{ name: "select", distance: 500 }],
       vehicle_name1: [],
       vehicle_name2: [],
       vehicle_name3: [],
@@ -82,22 +87,22 @@ export class FindFalconComponent implements OnInit {
   // submitting the form for find falcon
   findFalcon() {
     this.isSubmit = true;
-    console.log(this.findFalconForm.value.destination_1);
+    console.log(this.findFalconForm.value);
 
     if (this.findFalconForm.valid) {
       const planet_names = [];
       planet_names.push(
         this.findFalconForm.value.destination_1.name,
-        this.findFalconForm.value.destination_2,
-        this.findFalconForm.value.destination_3,
-        this.findFalconForm.value.destination_4
+        this.findFalconForm.value.destination_2.name,
+        this.findFalconForm.value.destination_3.name,
+        this.findFalconForm.value.destination_4.name
       );
       const vehicle_names = [];
       vehicle_names.push(
-        this.findFalconForm.value.vehicle_name1,
-        this.findFalconForm.value.vehicle_name2,
-        this.findFalconForm.value.vehicle_name3,
-        this.findFalconForm.value.vehicle_name4
+        this.findFalconForm.value.vehicle_name1.name,
+        this.findFalconForm.value.vehicle_name2.name,
+        this.findFalconForm.value.vehicle_name3.name,
+        this.findFalconForm.value.vehicle_name4.name
       );
       const postData = {
         token: localStorage.getItem("token"),
@@ -106,7 +111,7 @@ export class FindFalconComponent implements OnInit {
       };
       this.commonService.findFalcon(postData).subscribe((res: any) => {
         console.log(res);
-        if (status == "success") {
+        if (res.status == "success") {
           this.router.navigateByUrl(`success`);
         } else {
           this.router.navigateByUrl(`fail`);
@@ -114,6 +119,97 @@ export class FindFalconComponent implements OnInit {
       });
     } else {
       alert("Something went wrong");
+    }
+  }
+  // update vechicle count
+  updateVechileCount(vechile, event) {
+    console.log(vechile, event.target.name);
+    // vechile1
+    if (event.target.name === 'vehicle_name1') {
+      if (this.vechile1PrevId !== undefined) {
+           if (this.vechile1PrevId !== vechile.name) {
+             const index = this.vehicleData.findIndex(item => item.name === vechile.name);
+             const index2 = this.vehicleData.findIndex(item => item.name === this.vechile1PrevId);
+             console.log(index, index2);
+             const prevData = this.vehicleData[index2];
+             prevData.total_no = prevData.total_no + 1;
+             this.vehicleData[index2] = prevData;
+             vechile.total_no = vechile.total_no - 1;
+             this.vehicleData[index] = vechile;
+           }
+      } else {
+        const index = this.vehicleData.findIndex(item => item.name === vechile.name);
+        console.log(index);
+        vechile.total_no = vechile.total_no - 1;
+        this.vehicleData[index] = vechile;
+      }
+      this.vechile1PrevId = vechile.name;
+    }
+        // vechile2
+    if (event.target.name === 'vehicle_name2') {
+      if (this.vechile2PrevId !== undefined) {
+           if (this.vechile2PrevId !== vechile.name) {
+             const index = this.vehicleData.findIndex(item => item.name === vechile.name);
+             const index2 = this.vehicleData.findIndex(item => item.name === this.vechile2PrevId);
+             console.log(index, index2);
+             const prevData = this.vehicleData[index2];
+             prevData.total_no = prevData.total_no + 1;
+             this.vehicleData[index2] = prevData;
+             vechile.total_no = vechile.total_no - 1;
+             this.vehicleData[index] = vechile;
+           }
+      } else {
+        const index = this.vehicleData.findIndex(item => item.name === vechile.name);
+        console.log(index);
+        vechile.total_no = vechile.total_no - 1;
+        this.vehicleData[index] = vechile;
+      }
+      this.vechile2PrevId = vechile.name;
+
+    }
+    // vehicle3
+    if (event.target.name === 'vehicle_name3') {
+      if (this.vechile3PrevId !== undefined) {
+           if (this.vechile3PrevId !== vechile.name) {
+             const index = this.vehicleData.findIndex(item => item.name === vechile.name);
+             const index2 = this.vehicleData.findIndex(item => item.name === this.vechile3PrevId);
+             console.log(index, index2);
+             const prevData = this.vehicleData[index2];
+             prevData.total_no = prevData.total_no + 1;
+             this.vehicleData[index2] = prevData;
+             vechile.total_no = vechile.total_no - 1;
+             this.vehicleData[index] = vechile;
+           }
+      } else {
+        const index = this.vehicleData.findIndex(item => item.name === vechile.name);
+        console.log(index);
+        vechile.total_no = vechile.total_no - 1;
+        this.vehicleData[index] = vechile;
+      }
+      this.vechile3PrevId = vechile.name;
+
+    }
+     // vehicle4
+    if (event.target.name === 'vehicle_name4') {
+      if (this.vechile4PrevId !== undefined) {
+           if (this.vechile4PrevId !== vechile.name) {
+             const index = this.vehicleData.findIndex(item => item.name === vechile.name);
+             const index2 = this.vehicleData.findIndex(item => item.name === this.vechile4PrevId);
+             console.log(index, index2);
+             const prevData = this.vehicleData[index2];
+             prevData.total_no = prevData.total_no + 1;
+             this.vehicleData[index2] = prevData;
+             vechile.total_no = vechile.total_no - 1;
+             this.vehicleData[index] = vechile;
+           }
+      } else {
+        const index = this.vehicleData.findIndex(item => item.name === vechile.name);
+        console.log(index);
+        vechile.total_no = vechile.total_no - 1;
+        this.vehicleData[index] = vechile;
+      }
+      this.vechile4PrevId = vechile.name;
+
     }
   }
 }
